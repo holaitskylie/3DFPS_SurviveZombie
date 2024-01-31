a731315a94a6afd49be5e1e2b52370ee31f0b364
holaitskylie <june2840@naver.com>
2024-01-31 16:22:04 +0900

    Fixed Camera toration angle
    조이스틱으로 카메라 회전 시 앵글이 이상하게 돌아가는 문제 해결

--- Assets/Scripts/JoystickController.cs
+++ Assets/Scripts/JoystickController.cs
@@ -7,13 +7,15 @@ public class JoystickController : MonoBehaviour
 {
     public Joystick joystick;
     [SerializeField] private CinemachineVirtualCamera fpsCam;
+    [SerializeField] private GameObject player;
     [SerializeField] private float rotationHorizontal;
     [SerializeField] private float rotationVertical;
     [SerializeField] private float rotationSpeed = 35f;
     
     void Start()
     {
-        
+        if(player == null)
+            player = GameObject.Find("Player");        
     }
 
     // Update is called once per frame
@@ -21,8 +23,12 @@ public class JoystickController : MonoBehaviour
     {
         rotationVertical = joystick.Vertical;
         rotationHorizontal = joystick.Horizontal;
+        
+        //Point : Ä«¸Þ¶ó ¾Þ±Û
+        //fpsCam.transform.Rotate(-rotationVertical * rotationSpeed * Time.deltaTime, rotationHorizontal * rotationSpeed * Time.deltaTime, 0);
 
-        fpsCam.transform.Rotate(-rotationVertical * rotationSpeed * Time.deltaTime, rotationHorizontal * rotationSpeed * Time.deltaTime, 0);
+        player.transform.Rotate(0, rotationHorizontal * rotationSpeed * Time.deltaTime, 0);
+        fpsCam.transform.Rotate(-rotationVertical * rotationSpeed * Time.deltaTime, 0, 0);
         
     }
 }
